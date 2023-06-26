23dab81f1dbe044d3b0ba18a642fb1234a5c5e77
DE LA CRUZ CHICA ISAAC SALOMON <is_delacruz@casabaca.com>
2023-06-26 09:48:03 -0500

    Fin Seccion 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'bases';
+  public title:string = 'Hola Isaac';
+  public counter:number =10;
+
+  increaseBy(value:number):void{
+    this.counter+=value;
+  }
+
+  decreaseBy(value:number):void{
+    this.counter-=value;
+  }
+
+  resetCounter():void{
+    this.counter=10;
+  }
+
+
 }
